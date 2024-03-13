e300de7d2828d15930cc42ca9c97819324663e4d
Vinícius Miranda de Menezes <paravinicius@gmail.com>
2024-03-12 22:23:51 -0300

    Projeto inicial

---
+++ src/main/java/br/com/fiap/postech/hackathon2024/Hackathon2024Application.java
@@ -0,0 +1,13 @@
+package br.com.fiap.postech.hackathon2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackathon2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hackathon2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/postech/hackathon2024/Hackathon2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.postech.hackathon2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackathon2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
