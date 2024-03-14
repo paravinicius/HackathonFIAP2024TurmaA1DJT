8a8ced73f41493793730340446a1426baf92ca04
Lucca Brito <luccabritogt@gmail.com>
2024-03-13 23:57:12 -0300

    Criação do script para popular o banco de dados e ajuste no application.properties

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO servico_item (nome, valor) VALUES ('Massagem Completa', 250.00);
+INSERT INTO servico_item (nome, valor) VALUES ('Café da Manhã', 65.00);
+INSERT INTO servico_item (nome, valor) VALUES ('Manicure', 85.00);
+INSERT INTO servico_item (nome, valor) VALUES ('Almoço', 65.00);
+INSERT INTO servico_item (nome, valor) VALUES ('Jantar', 85.00);
+INSERT INTO servico_item (nome, valor) VALUES ('Refrigerantes', 8.00);
+INSERT INTO servico_item (nome, valor) VALUES ('Sucos', 11.00);
+INSERT INTO servico_item (nome, valor) VALUES ('Cervejas Nacionais', 12.00);
+INSERT INTO servico_item (nome, valor) VALUES ('Cervejas Importadas', 20.00);
+INSERT INTO servico_item (nome, valor) VALUES ('Caipiroska', 25.00);
