2544805b127bb03490f9a8f1c52162cc1a5a9183
oFrancoGui <oguilhermefranco@gmail.com>
2024-03-13 22:26:22 -0300

    correcao service

--- src/main/java/br/com/fiap/postech/hackathon2024/servicositens/services/ServicosItensService.java
+++ src/main/java/br/com/fiap/postech/hackathon2024/servicositens/services/ServicosItensService.java
@@ -1,14 +1,18 @@
 package br.com.fiap.postech.hackathon2024.servicositens.services;
 
 import br.com.fiap.postech.hackathon2024.servicositens.entities.ServicosItens;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ServicosItensService {
 
     public ServicosItens criarServicosItens(ServicosItens servicosItens) {
+        return null;
     }
 
     public List<ServicosItens> buscarTodosServicosItens() {
+        return null;
     }
 }
