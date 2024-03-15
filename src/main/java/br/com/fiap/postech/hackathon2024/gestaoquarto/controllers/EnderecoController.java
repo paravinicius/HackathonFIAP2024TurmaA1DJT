726132b2ad36ebaeaa951b0b6c9fd149c0536f5c
Vinícius Miranda de Menezes <paravinicius@gmail.com>
2024-03-15 19:10:01 -0300

    Criacao do Endpoint de Endereco

--- src/main/java/br/com/fiap/postech/hackathon2024/gestaoquarto/controllers/EnderecoController.java
+++ src/main/java/br/com/fiap/postech/hackathon2024/gestaoquarto/controllers/EnderecoController.java
@@ -1,14 +1,15 @@
 package br.com.fiap.postech.hackathon2024.gestaoquarto.controllers;
 
 import br.com.fiap.postech.hackathon2024.gestaoquarto.entities.Endereco;
+import br.com.fiap.postech.hackathon2024.gestaoquarto.entities.Quarto;
 import br.com.fiap.postech.hackathon2024.gestaoquarto.services.EnderecoService;
+import br.com.fiap.postech.hackathon2024.gestaoservicositens.exceptions.ServicoItemNaoEncontradoException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/enderecos")
@@ -24,6 +25,26 @@ public class EnderecoController {
     public ResponseEntity<Endereco> criarEndereco(@RequestBody Endereco enderecoRequest) {
         Endereco enderecoNovo = enderecoService.criarQuarto(enderecoRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(enderecoNovo);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Endereco>> buscarTodosEnderecos() {
+        List<Endereco> endereco = enderecoService.buscarTodosEnderecos();
+        return ResponseEntity.ok(endereco);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Endereco> atualizarEndereco(@PathVariable Long id, @RequestBody Endereco endereco) {
+        try {
+            Endereco enderecoAtualizado = enderecoService.atualizarEndereco(id, endereco);
+            if (enderecoAtualizado != null) {
+                return ResponseEntity.ok(enderecoAtualizado);
+            }
+        }
+        catch (RuntimeException e) {
+            throw new ServicoItemNaoEncontradoException(id);
+        }
+        return ResponseEntity.notFound().build();
     }
 
 }
--- src/main/java/br/com/fiap/postech/hackathon2024/gestaoquarto/services/EnderecoService.java
+++ src/main/java/br/com/fiap/postech/hackathon2024/gestaoquarto/services/EnderecoService.java
@@ -6,6 +6,9 @@ import br.com.fiap.postech.hackathon2024.gestaoquarto.repositories.EnderecoRepos
 import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class EnderecoService {
@@ -21,5 +24,28 @@ public class EnderecoService {
     }
 
     public Endereco criarQuarto(Endereco endereco){  return enderecoRepository.save(endereco); }
+
+    public List<Endereco> buscarTodosEnderecos() {
+        return enderecoRepository.findAll();
+    }
+
+    public Endereco buscarEnderecoPorId(Long id) {
+        return enderecoRepository.findById(id).orElse(null);
+    }
+
+    public Endereco atualizarEndereco(Long id, Endereco endereco) {
+        Endereco enderecoExistente = enderecoRepository.findById(id).orElse(null);
+        if (
+            endereco != null &&
+            enderecoExistente != null &&
+            enderecoExistente.getId() != null &&
+            endereco.getId() != null &&
+            enderecoExistente.getId().intValue() == endereco.getId().intValue()
+        ) {
+            return enderecoRepository.save(endereco);
+        } else {
+            throw new RuntimeException();
+        }
+    }
 
 }
